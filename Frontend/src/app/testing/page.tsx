96c64eac48de82be68cc92dab191cbf376e0e875
Tomas McGuinness <tomas@tomasmcguinness.com>
2025-08-30 18:14:57 +0100

    Add configuration page for the tariff so slots can be changed

--- Frontend/src/app/testing/page.tsx
+++ Frontend/src/app/testing/page.tsx
@@ -1,71 +1,48 @@
 'use client'
 
+import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
-
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
-import { useState } from 'react';
-import { getEnergyManager } from '../../../energyManager.js';
-
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-);
 
 export default function Page() {
 
-   const [chartData, setChartData] = useState({
-        labels: [10,20,30,40,50],
-        datasets: [
-            {
-                label: 'Real-time Data',
-                data: [[30,50],[40,50],[50,50]],
-                fill: false,
-            },
-        ],
+    const [tariffStructure, setTariffStructure] = useState<any[]>([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/tariff/configuration').then(r => r.json()).then(data => { setTariffStructure(data); });
+    }, []);
+
+    const updateTariff = async (sender: any) => {
+        console.log(sender.target.dataset.index);
+
+        tariffStructure[sender.target.dataset.index].price = tariffStructure[sender.target.dataset.index].price == 7.5 ? 28 : 7.5;
+        setTariffStructure([...tariffStructure]);
+
+        fetch(`http://localhost:3000/tariff/configuration?id=${sender.target.dataset.index}`, { method: "POST" });
+    }
+
+    const rows = tariffStructure.map((tariff: any, index: number) => {
+        return <tr key={index}>
+            <td>{tariff.hour}</td><td>{tariff.startMinute}</td><td>{tariff.endMinute}</td><td><input data-index={index} type="checkbox" onChange={updateTariff} checked={tariff.price === 7.5} /></td>
+        </tr>
     });
-
-    const addForecast = () => {
-        let energyManager = getEnergyManager();
-
-        if(energyManager) {
-            console.log({energyManager});
-            energyManager.processForecast(1, {startTime: 1, endTime: 2});
-        }
-    };
-
-    const options = {
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'top' as const,
-            },
-            title: {
-                display: false
-            },
-        },
-    };
 
     return <Container>
         <div>
-            Test Page
+            Tariff Configuration
             <hr />
-            <Line options={options} data={chartData} />
-            <button onClick={addForecast}>Add Forecast</button>
+            <table className="table bordered table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Hour</th>
+                        <th>Start Minute</th>
+                        <th>End Minute</th>
+                        <th>Peak</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
         </div>
     </Container>;
 }
